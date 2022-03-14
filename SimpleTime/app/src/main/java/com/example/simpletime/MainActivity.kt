392b7aee44d05d5e2d8d8674ff5148591907b083
PavlikStudies <paulius.klimaitis@ktu.edu>
2022-03-14 14:44:30 +0200

    Singup/Login veikiantys ir pasalintas Amplify

--- SimpleTime/app/src/main/java/com/example/simpletime/MainActivity.kt
+++ SimpleTime/app/src/main/java/com/example/simpletime/MainActivity.kt
@@ -3,14 +3,10 @@ package com.example.simpletime
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.amplifyframework.AmplifyException
-import com.amplifyframework.api.aws.AWSApiPlugin
-import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
-import com.amplifyframework.core.Amplify
-import com.amplifyframework.datastore.AWSDataStorePlugin
-import com.amplifyframework.datastore.generated.model.Users
-import com.amplifyframework.storage.s3.AWSS3StoragePlugin
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_first.*
 import java.io.File
 
@@ -19,120 +15,39 @@ class MainActivity : AppCompatActivity() {
 
 //    private lateinit var appBarConfiguration: AppBarConfiguration
 //    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_createAcc.setOnClickListener{
+        auth = FirebaseAuth.getInstance()
+
+
+        login_btnCreateAcc.setOnClickListener{
             val intent = Intent(this, SecondFragment::class.java);
             startActivity(intent)
+
         }
 
-        try {
-            Amplify.addPlugin(AWSApiPlugin())
-            Amplify.addPlugin(AWSDataStorePlugin())
-            Amplify.addPlugin(AWSCognitoAuthPlugin())
-            Amplify.addPlugin(AWSS3StoragePlugin())
-            Amplify.configure(applicationContext)
-
-            Log.i("Tutorial", "Initialized Amplify")
-        } catch (failure: AmplifyException) {
-            Log.e("Tutorial", "Could not initialize Amplify", failure)
+        login_btnLogin.setOnClickListener{
+         login(it)
         }
-        Amplify.Auth.fetchAuthSession(
-            { Log.i("AmplifyQuickstart", "Auth session = $it") },
-            { error -> Log.e("AmplifyQuickstart", "Failed to fetch auth session", error) }
-        )
-        Amplify.Auth.signIn("jonasmargirisklimas@gmail.com", "Password123$",
-            { result ->
-                if (result.isSignInComplete) {
-                    Log.i("AuthQuickstart", "Sign in succeeded")
-                } else {
-                    Log.i("AuthQuickstart", "Sign in not complete")
-                }
-            },
-            { Log.e("AuthQuickstart", "Failed to sign in", it) }
-        )
-//        super.onCreate(savedInstanceState)
 
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        setSupportActionBar(binding.toolbar)
-//
-//        val navController = findNavController(R.id.nav_host_fragment_content_main)
-//        appBarConfiguration = AppBarConfiguration(navController.graph)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//
-//        binding.fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-//        }
-        val item = Users.builder()
-            .usrname("gamer69")
-            .passw("123123")
-            .email("asdasd@gmail.com")
-            .build()
-//        Amplify.DataStore.save(item,
-//            { Log.i("Tutorial", "Saved item: ${item.usrname}") },
-//            { Log.e("Tutorial", "Could not save item to DataStore", it) }
-//        )
-        Amplify.DataStore.observe(Users::class.java,
-            { Log.i("Tutorial", "Observation began.") },
-            { Log.i("Tutorial", it.item().toString()) },
-            { Log.e("Tutorial", "Observation failed.", it) },
-            { Log.i("Tutorial", "Observation complete.") }
-        )
-        uploadFile()
-        Amplify.DataStore.query(Users::class.java,
-            { todos ->
-                while (todos.hasNext()) {
-                    val Users: Users = todos.next()
-                    Log.i("Tutorial", "==== Todo ====")
-                    Log.i("Tutorial", "Name: ${Users.usrname}")
-                    Log.i("Tutorial", "Priority: ${Users.passw}")
-                    Log.i("Tutorial", "CompletedAt: ${Users.email}")
-                }
-            },
-            { Log.e("Tutorial", "Could not query DataStore", it)  }
-        )
-//        val options = AuthSignUpOptions.builder()
-//            .userAttribute(AuthUserAttributeKey.email(), "jonasmargirisklimas@gmail.com")
-//            .userAttribute(AuthUserAttributeKey.nickname(), "XDXDXDXDXD")
-//            .build()
-//        Amplify.Auth.signUp("jonasmargirisklimas@gmail.com", "Password123$", options,
-//            { Log.i("AuthQuickStart", "Sign up succeeded: $it") },
-//            { Log.e ("AuthQuickStart", "Sign up failed", it) }
-//        )
-//        Amplify.Auth.confirmSignUp(
-//            "jonasmargirisklimas@gmail.com", "331140",
-//            { result ->
-//                if (result.isSignUpComplete) {
-//                    Log.i("AuthQuickstart", "Confirm signUp succeeded")
-//                } else {
-//                    Log.i("AuthQuickstart","Confirm sign up not complete")
-//                }
-//            },
-//            { Log.e("AuthQuickstart", "Failed to confirm sign up", it) }
-//        )
+    }
+    fun login(view: View){
+        val email=login_enterName.text.toString()
+        val password=login_enterPassword.text.toString()
+
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                val intent= Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener { exception ->
+            Toast.makeText(applicationContext,exception.localizedMessage, Toast.LENGTH_LONG).show()
+        }
     }
 
-    private fun uploadFile() {
-        val exampleFile = File(applicationContext.filesDir, "ExampleKey")
-        exampleFile.writeText("Example file contents")
-
-        Amplify.Storage.uploadFile("ExampleKey", exampleFile,
-            { Log.i("MyAmplifyApp", "Successfully uploaded: ${it.key}") },
-            { Log.e("MyAmplifyApp", "Upload failed", it) }
-        )
     }
-
-
-
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navController = findNavController(R.id.nav_host_fragment_content_main)
-//        return navController.navigateUp(appBarConfiguration)
-//                || super.onSupportNavigateUp()
-//    }
-}
--- SimpleTime/app/src/main/java/com/example/simpletime/SecondFragment.kt
+++ SimpleTime/app/src/main/java/com/example/simpletime/SecondFragment.kt
@@ -3,18 +3,49 @@ package com.example.simpletime
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_first.*
 import kotlinx.android.synthetic.main.fragment_second.*
 
+
+private lateinit var auth: FirebaseAuth
 class SecondFragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_second)
 
+        auth = FirebaseAuth.getInstance()
 
         signup_btnBack.setOnClickListener{
+
             val intent = Intent(this, MainActivity::class.java);
             startActivity(intent)
+
+        }
+        signup_btnSignup.setOnClickListener{
+            register(it)
         }
     }
+
+    fun register(view: View)
+    {
+        val email = signup_enterEmail.text.toString()
+        val name = signup_enterName.text.toString()
+        val password = signup_enterPassword.text.toString();
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener() {      task ->
+
+        if(task.isSuccessful)
+            {
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener { exception ->
+            Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
