601cc823d2dc992e3ef183b0e0ce2b9e32d3c420
PavlikStudies <paulius.klimaitis@ktu.edu>
2022-03-14 13:25:09 +0200

    Klase

---
+++ SimpleTime/app/src/main/java/com/example/simpletime/userControlJava.java
@@ -0,0 +1,14 @@
+package com.example.simpletime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userControlJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_control_java);
+    }
+}
