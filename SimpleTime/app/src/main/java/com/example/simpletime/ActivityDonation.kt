3a742c6c8eea65b17500fcacfa66c46b62fdf6b3
ugniusado <60697428+ugniusado@users.noreply.github.com>
2022-03-20 17:56:16 +0200

    donation and save to device pop-up

---
+++ SimpleTime/app/src/main/java/com/example/simpletime/ActivityDonation.kt
@@ -0,0 +1,11 @@
+package com.example.simpletime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityDonation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donation)
+    }
+}
