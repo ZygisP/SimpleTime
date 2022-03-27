205e862cecfdcc6473c8fcab0b2e9830fbbfa1f2
margaXD <58165730+margaXD@users.noreply.github.com>
2022-03-27 13:35:11 +0300

    Rate animation and trash popup

--- SimpleTime/app/src/main/java/com/example/simpletime/ActivitySaveVideo.kt
+++ SimpleTime/app/src/main/java/com/example/simpletime/ActivitySaveVideo.kt
@@ -5,19 +5,24 @@ import android.animation.ValueAnimator
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.core.graphics.ColorUtils
 import kotlinx.android.synthetic.main.activity_rate_popup.*
+import kotlinx.android.synthetic.main.activity_save_video.*
 
 class ActivitySaveVideo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_save_video)
-        val alpha = 100 //between 0-255
-        val alphaColor = ColorUtils.setAlphaComponent(Color.parseColor("#000000"), alpha)
-        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), Color.TRANSPARENT, alphaColor)
-        colorAnimation.duration = 500 // milliseconds
-        colorAnimation.addUpdateListener { animator ->
-            popup_window_background.setBackgroundColor(animator.animatedValue as Int)
+        button.setOnClickListener{
+            var popup = PopupMenu(this, button)
+            popup.inflate(R.menu.savepopup)
+            popup.setOnMenuItemClickListener {
+                Toast.makeText(this, "Item : " + it.title, Toast.LENGTH_SHORT).show()
+                true
+            }
+            popup.show()
         }
     }
 }
