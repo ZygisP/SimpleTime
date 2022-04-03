45917af7ca950c5860e1b56f9d081c4e17d81878
ugniusado <60697428+ugniusado@users.noreply.github.com>
2022-04-03 17:03:18 +0300

    Creator-profile front

---
+++ SimpleTime/app/src/main/java/com/example/simpletime/CreatorsProfile1.kt
@@ -0,0 +1,11 @@
+package com.example.simpletime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreatorsProfile1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creators_profile1)
+    }
+}
