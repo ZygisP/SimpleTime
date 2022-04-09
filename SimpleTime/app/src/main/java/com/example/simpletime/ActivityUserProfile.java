d372fb1531364bc162f0c5693ba2d04d65f41bcb
arnas78 <79596248+arnas78@users.noreply.github.com>
2022-04-09 20:18:02 +0300

    Implemented an ability to change user profile name and email

--- SimpleTime/app/src/main/java/com/example/simpletime/ActivityUserProfile.java
+++ SimpleTime/app/src/main/java/com/example/simpletime/ActivityUserProfile.java
@@ -1,22 +1,34 @@
 package com.example.simpletime;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -28,21 +40,35 @@ import java.util.UUID;
 public class ActivityUserProfile extends AppCompatActivity {
 
     private ImageView profileImage;
+
     private Uri imageUri;
     private FirebaseStorage storage;
     private StorageReference storageReference;
     private FirebaseAuth fAuth;
+    EditText profileName, profileEmail;
+    Button btnBack;
 
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
         profileImage = findViewById(R.id.userProfile_profilePicture);
+        profileName = findViewById(R.id.userProfile_editName);
+        profileEmail = findViewById(R.id.userProfile_editEmail);
+        btnBack = findViewById(R.id.userProfile_btnBack);
 
         fAuth = FirebaseAuth.getInstance();
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
+
+
+        if (user != null) {
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+        }
 
         loadImagePlaceholder();
         StorageReference profileRef = storageReference.child("users/" + fAuth.getCurrentUser().getUid() + "/profile.jpg");
@@ -60,7 +86,97 @@ public class ActivityUserProfile extends AppCompatActivity {
             }
         });
 
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        showAllUserData();
     }
+
+
+    private void showAllUserData(){
+        profileName.setText(user.getDisplayName());
+        profileEmail.setText(user.getEmail());
+    }
+
+    boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+
+    public void update_profile(View view){
+        if(isNameChanged() || isEmailChanged()){
+            Toast.makeText(this,"Data has been updated",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this,"Saved data. Nothing to update.",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isEmailChanged() {
+
+        if (!user.getEmail().equals(profileEmail.getText().toString()) &&
+                isEmailValid(profileEmail.getText().toString()))
+        {
+            user.updateEmail(profileEmail.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "User email address updated.");
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "User email was not updated.");
+                        }
+                    });
+            return true;
+        }
+        else if (!user.getEmail().equals(profileEmail.getText().toString()) &&
+                !isEmailValid(profileEmail.getText().toString()))
+        {
+            Toast.makeText(this,"Failed to update info, email is invalid.",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else {
+            return false;
+        }
+
+
+
+    }
+
+    private boolean isNameChanged() {
+
+        if (!user.getDisplayName().equals(profileName.getText().toString())){
+
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(profileName.getText().toString())
+                    .build();
+
+            user.updateProfile(profileUpdates)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "User profile updated.");
+                            }
+                        }
+                    });
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
 
     private void choosePicture(){
         Intent intent = new Intent();
