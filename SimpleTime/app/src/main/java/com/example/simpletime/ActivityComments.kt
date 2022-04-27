da1c0ac2869ec412f84596983473e4ce7619a15e
margaXD <58165730+margaXD@users.noreply.github.com>
2022-04-27 16:33:04 +0300

    videopage updates

--- SimpleTime/app/src/main/java/com/example/simpletime/ActivityComments.kt
+++ SimpleTime/app/src/main/java/com/example/simpletime/ActivityComments.kt
@@ -3,10 +3,12 @@ package com.example.simpletime
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_comments.*
 
 class ActivityComments : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        var db = FirebaseFirestore.getInstance()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comments)
         imageButton.setOnClickListener {
