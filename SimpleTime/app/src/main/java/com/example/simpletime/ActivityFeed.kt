4d909e65f25770cc58643a3a713aa42b2c6d5522
margaXD <58165730+margaXD@users.noreply.github.com>
2022-05-18 14:51:47 +0300

    feed work

--- SimpleTime/app/src/main/java/com/example/simpletime/ActivityFeed.kt
+++ SimpleTime/app/src/main/java/com/example/simpletime/ActivityFeed.kt
@@ -3,12 +3,14 @@ package com.example.simpletime
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ListResult
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_feed.*
 import kotlin.random.Random
 
@@ -28,87 +30,100 @@ class ActivityFeed : AppCompatActivity() {
 
 
 //        loadImagePlaceholder();
-        val profileRef =
-            storageReference.child("videos/")
-        val listRef = FirebaseStorage.getInstance().getReference("Videos/")
         val listRefVids = FirebaseStorage.getInstance().getReference("Videos/")
-
-        listRef.listAll()
+        listRefVids.listAll()
             .addOnSuccessListener(OnSuccessListener<ListResult> { listResult ->
-                Log.d(TAG, listRef.getName())
-                for (prefix in listResult.prefixes) {
-                    Log.d(TAG, "Folders here: " + prefix.name)
-                    val listRefVids = FirebaseStorage.getInstance().getReference("Videos/" + prefix.name + "/")
-                    listRefVids.listAll()
-                        .addOnSuccessListener(OnSuccessListener<ListResult> { listResult ->
-                            Log.d(TAG, listRefVids.getName())
-                            for (item in listResult.items) {
-                                i = i + 1
-                                var vidname = item.name.dropLast(4)
-                                Log.d(TAG, "Videos here:" + vidname)
-                                rand1 = (0..i).random()
-                                rand2 = (0..i).random()
-                                rand3 = (0..i).random()
-                                Log.d(TAG, "chosen videos:" + rand1 + " " + rand2 + " " + rand3)
-                                val chooseVids = FirebaseStorage.getInstance().getReference("Videos/" + prefix.name+"/")
-                                chooseVids.listAll()
-                                    .addOnSuccessListener { OnSuccessListener<ListResult> { listResult ->
-                                        for(item in listResult.items) {
-                                            Log.d(TAG, "Checking " + item.name)
-                                            j = j + 1
-                                            if (j == rand1){
-                                                var vidname1 = item.name.dropLast(4)
-                                                val docRef = db.collection("videos").document(vidname1)
-                                                docRef.get()
-                                                    .addOnSuccessListener { document ->
-                                                        if (document != null) {
-                                                            Log.d(TAG, "Pulled vide:" + vidname1)
-                                                            textView5.text = document.getString("title")
-                                                        }
-                                            }
-                                                    .addOnFailureListener{
-                                                        Log.d(TAG, "nosuchdoc for 1")
-                                                    }
-                                        }
-                                            else if (j == rand2){
-                                                var vidname1 = item.name.dropLast(4)
-                                                val docRef = db.collection("videos").document(vidname1)
-                                                docRef.get()
-                                                    .addOnSuccessListener { document ->
-                                                        if (document != null) {
-                                                            Log.d(TAG, "Pulled vide:" + vidname1)
-                                                            textView10.text = document.getString("title")
-                                                        }
-                                                    }
-                                                    .addOnFailureListener{
-                                                        Log.d(TAG, "nosuchdoc for 1")
-                                                    }
-                                            }
-                                            else if (j == rand3){
-                                                var vidname1 = item.name.dropLast(4)
-                                                val docRef = db.collection("videos").document(vidname1)
-                                                docRef.get()
-                                                    .addOnSuccessListener { document ->
-                                                        if (document != null) {
-                                                            Log.d(TAG, "Pulled vide:" + vidname1)
-                                                            textView14.text = document.getString("title")
-                                                        }
-                                                    }
-                                                    .addOnFailureListener{
-                                                        Log.d(TAG, "nosuchdoc for 1")
-                                                    }
-                                            }
-                                        }
-                                    } }
-                            }
-                        })
+                Log.d(TAG, listRefVids.getName())
+                for (item in listResult.items){
+                    i=i+1
+                    rand1 = (1..i).random()
+                    rand2 = (1..i).random()
+                    rand3 = (1..i).random()
+                    if(rand1 == rand2 || rand2 == rand3 || rand1 == rand3) {
+                        rand1 = (1..i).random()
+                        rand2 = (1..i).random()
+                        rand3 = (1..i).random()
+                    }
+                    if(rand1 == rand2 || rand2 == rand3 || rand1 == rand3) {
+                        rand1 = (1..i).random()
+                        rand2 = (1..i).random()
+                        rand3 = (1..i).random()
+                    }
+                    if(rand1 == rand2 || rand2 == rand3 || rand1 == rand3) {
+                        rand1 = (1..i).random()
+                        rand2 = (1..i).random()
+                        rand3 = (1..i).random()
+                    }
                 }
+                Log.d(TAG, "chosen videos:" + rand1 + " " + rand2 + " " + rand3)
                 for (item in listResult.items) {
-                    Log.d(TAG, "Aint no folders here")
-                }
-            })
-            .addOnFailureListener(OnFailureListener {
-                // Uh-oh, an error occurred!
-            })
+                    j = j +1
+                    var vidname = item.name.dropLast(4)
+                    Log.d(TAG, "Videos here:" + vidname)
+                    Log.d(TAG, "Checking " + item.name)
+                    if (j == rand1){
+                        val docRef = db.collection("videos").document(vidname)
+                        docRef.get()
+                            .addOnSuccessListener { document ->
+                                if (document != null) {
+                                    Log.d(TAG, "Pulled video for 1:" + vidname)
+                                    textView5.text = document.getString("title")
+                                    val uploaderPic = document.getString("uploaderID")
+                                    val profileImage = findViewById<ImageView>(R.id.imageView8)
+                                    val profileRef = storageReference.child(
+                                        "users/" + uploaderPic + "/profile.jpg"
+                                    )
+                                    profileRef.downloadUrl.addOnSuccessListener { uri ->
+                                        Picasso.get().load(uri).into(profileImage)
+                                    }
+                                }
+                            }
+                            .addOnFailureListener{
+                                Log.d(TAG, "nosuchdoc for 1")
+                            }
+                    }
+                    else if (j == rand2){
+                        val docRef = db.collection("videos").document(vidname)
+                        docRef.get()
+                            .addOnSuccessListener { document ->
+                                if (document != null) {
+                                    Log.d(TAG, "Pulled video for 2:" + vidname)
+                                    textView10.text = document.getString("title")
+                                    val uploaderPic = document.getString("uploaderID")
+                                    val profileImage = findViewById<ImageView>(R.id.imageView19)
+                                    val profileRef = storageReference.child(
+                                        "users/" + uploaderPic + "/profile.jpg"
+                                    )
+                                    profileRef.downloadUrl.addOnSuccessListener { uri ->
+                                        Picasso.get().load(uri).into(profileImage)
+                                    }
+                                }
+                            }
+                            .addOnFailureListener{
+                                Log.d(TAG, "nosuchdoc for 1")
+                            }
+                    }
+                    else if (j == rand3){
+                        val docRef = db.collection("videos").document(vidname)
+                        docRef.get()
+                            .addOnSuccessListener { document ->
+                                if (document != null) {
+                                    Log.d(TAG, "Pulled video for 3:" + vidname)
+                                    textView14.text = document.getString("title")
+                                    val uploaderPic = document.getString("uploaderID")
+                                    val profileImage = findViewById<ImageView>(R.id.imageView23)
+                                    val profileRef = storageReference.child(
+                                        "users/" + uploaderPic + "/profile.jpg"
+                                    )
+                                    profileRef.downloadUrl.addOnSuccessListener { uri ->
+                                        Picasso.get().load(uri).into(profileImage)
+                                    }
+                                }
+                            }
+                            .addOnFailureListener{
+                                Log.d(TAG, "nosuchdoc for 1")
+                            }
+                    }
+                } })
     }
     }
